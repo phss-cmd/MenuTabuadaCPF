86e1a8d77d9aef03edeba5444c23a34270ebf37d
phss-cmd <ps12.ps63@gmail.com>
2022-05-05 12:01:20 -0300

    Adicionar arquivos de projeto.

---
+++ MenuTabuadaCPF/View/Tabuada/TelaTabuada.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MenuTabuadaCPF.View.Tabuada
+{
+    public partial class TelaTabuada : Form
+    {
+        public TelaTabuada()
+        {
+            InitializeComponent();
+        }
+
+        private void CalcularTabuada()
+            {
+                 int numero = 0;
+                 
+
+            numero = Convert.ToInt32(tbxNumero.Text);
+
+
+            for (int i = 0; i < 10; i++)
+            {
+                lbxResultado.Items.Add(numero + " x " + i + " = " + i * numero);
+            }
+            lbxResultado.Focus();
+            }
+        private void LimparTela()
+        {
+            lbxResultado.Items.Clear();
+  
+        }
+
+        private void tbxNumero_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 3)
+            {
+                LimparTela();
+            CalcularTabuada(); 
+        }
+        }
+    }
+}
